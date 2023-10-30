0aa972290dfb731e7f8b1373e4f96d7195ce8589
Vusal0013 <vusal.feyruz@gmail.com>
2023-10-30 19:46:41 +0400

    add font and reset css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      r01: ["Poppins", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
