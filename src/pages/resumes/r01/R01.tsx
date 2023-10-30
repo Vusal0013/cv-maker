b7cbf7f1de6a5eb0d9894bdc8431e3c633b2ce36
Vusal0013 <vusal.feyruz@gmail.com>
2023-10-30 19:55:44 +0400

    create Resume01 R01 skeleton

--- src/pages/resumes/r01/R01.tsx
+++ src/pages/resumes/r01/R01.tsx
@@ -1,13 +1,13 @@
-import {
-  PersonalInformation,
-  ProfessionalInformation,
-} from "../../../components/r01/";
+import React from "react";
+import { PersonalInfo, ProfessionalInfo } from "../../../components/r01/";
 
-const R01 = () => {
+const R01: React.FC = () => {
   return (
     <div className="bg-[lightblue] flex flex-col justify-center items-center min-h-screen w-full">
-      <ProfessionalInformation />
-      <PersonalInformation />
+      <div className="w-full max-w-[100rem] min-h-[50rem] bg-white flex shadow-[0_3.5rem_5.5rem_rgba(0,0,0,0.2)] m-20">
+        <PersonalInfo />
+        <ProfessionalInfo />
+      </div>
     </div>
   );
 };
