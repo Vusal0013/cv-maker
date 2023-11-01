074350477f81a0b10600b86f15d64b52fe4639e6
Vusal0013 <vusal.feyruz@gmail.com>
2023-11-02 02:16:55 +0400

    Updated styling in R01 component

--- src/pages/resumes/r01/R01.tsx
+++ src/pages/resumes/r01/R01.tsx
@@ -4,7 +4,7 @@ import { PersonalInfo, ProfessionalInfo } from "../../../components/r01/";
 const R01: React.FC = () => {
   return (
     <div className="bg-[lightblue] flex flex-col justify-center items-center min-h-screen w-full">
-      <div className="w-full max-w-[100rem] min-h-[50rem] bg-white flex shadow-[0_3.5rem_5.5rem_rgba(0,0,0,0.2)] m-20">
+      <div className="w-full max-w-5xl min-h-screen bg-white flex shadow-2xl m-12">
         <PersonalInfo />
         <ProfessionalInfo />
       </div>
