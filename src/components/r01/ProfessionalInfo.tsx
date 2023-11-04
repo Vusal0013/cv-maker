546361d62b9a2c6fc9e8c5083850a193fa816450
Vusal0013 <vusal.feyruz@gmail.com>
2023-11-04 17:24:49 +0400

    added ProfessionalSkills component

--- src/components/r01/ProfessionalInfo.tsx
+++ src/components/r01/ProfessionalInfo.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { About, WorkExperience } from ".";
+import { About, ProfessionalSkills, WorkExperience } from ".";
 
 const ProfessionalInfo: React.FC = () => {
   return (
     <div className="bg-white w-2/3 pt-10 pb-8 px-8">
       <About />
       <WorkExperience />
+      <ProfessionalSkills />
     </div>
   );
 };
