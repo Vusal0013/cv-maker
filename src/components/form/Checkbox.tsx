9bbd4c9f2712743ed7af233e3bb8918721cd72c3
Vusal0013 <vusal.feyruz@gmail.com>
2023-12-02 02:17:44 +0400

    created Checkbox component

---
+++ src/components/form/Checkbox.tsx
@@ -0,0 +1,33 @@
+import { FieldHookConfig, useField } from "formik";
+import React from "react";
+import { FaCheck } from "react-icons/fa6";
+
+interface ICheckbox {
+  label: string;
+}
+
+const Checkbox: React.FC<ICheckbox & FieldHookConfig<any>> = ({
+  label,
+  ...props
+}) => {
+  const [field, , helpers] = useField(props);
+
+  const handleSetVal = () => {
+    helpers.setValue(!field.value);
+  };
+  return (
+    <div className="flex justify-between w-max gap-2 items-center">
+      <div
+        onClick={handleSetVal}
+        className="bg-white w-5 h-5 flex justify-center items-center border-[cdcdcd] border-2 text-[#2e2e2e]"
+      >
+        {!field.value && <FaCheck size={13} />}
+      </div>
+      <label className="select-none" onClick={handleSetVal}>
+        {label}
+      </label>
+    </div>
+  );
+};
+
+export { Checkbox };
