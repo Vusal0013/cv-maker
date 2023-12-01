291533b900baeeb7a98ec2998bee35771b26956d
Vusal0013 <vusal.feyruz@gmail.com>
2023-12-02 02:13:52 +0400

    added birthdate in resume

--- src/components/r01/ProfileInfo.tsx
+++ src/components/r01/ProfileInfo.tsx
@@ -8,7 +8,7 @@ const ProfileInfo: React.FC = () => {
     <div className="flex flex-col items-center pb-5 border-b-[rgba(255,255,255,0.2)] border-b border-solid">
       <div className="relative min-w-[12rem] min-h-[12rem] max-w-[12rem] max-h-48 overflow-hidden rounded-[50%]">
         <img
-          src={profileInfo.profilePhoto}
+          src={profileInfo.profilePhoto?.src}
           alt="Profile photo"
           className="absolute w-full h-full object-cover left-0 top-0;"
         />
@@ -16,7 +16,12 @@ const ProfileInfo: React.FC = () => {
       <h1 className="text-white text-2xl uppercase text-center font-semibold mt-5">
         {profileInfo.firstName} {profileInfo.lastName}
       </h1>
-      <small className="text-[#ffffffaa]">{profileInfo.birthDate}</small>
+      {profileInfo.birthDate.isRequired && (
+        <small className="text-[#ffffffaa]">
+          {profileInfo.birthDate.day} {profileInfo.birthDate.month}{" "}
+          {profileInfo.birthDate.year}
+        </small>
+      )}
       <h2 className="text-xl font-light text-white uppercase text-center mt-5">
         {profileInfo.profession}
       </h2>
