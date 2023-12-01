839a613d5cdab0d128d919bd0f6b481681b01ed8
Vusal0013 <vusal.feyruz@gmail.com>
2023-12-02 02:12:48 +0400

    exporting a new data

--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -5,3 +5,11 @@ export { Input } from "./Input";
 export { InputError } from "./InputError";
 export { PhotoFile } from "./PhotoFile";
 export { ResumeForm } from "./ResumeForm";
+export { Checkbox } from "./Checkbox";
+export { InputAnimation } from "./InputAnimation";
+
+//Yup Validation schema
+export { validationSchema } from "./validationSchema";
+//Formik initial Values
+
+export { initialValues } from "./initialValues";
