8d66d35030c00990f7fc954b77fa627bf4907c96
Vusal0013 <vusal.feyruz@gmail.com>
2023-12-02 02:08:29 +0400

    create Error components for inputs

--- src/components/form/InputError.tsx
+++ src/components/form/InputError.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const InputError: React.FC<{ children?: string }> = ({ children }) => {
-  return <div>{children}</div>;
+const InputError: React.FC<{ children: string }> = ({ children }) => {
+  return <div className="w-full h-5 text-red-800 text-sm mt-1">{children}</div>;
 };
 
 export { InputError };
