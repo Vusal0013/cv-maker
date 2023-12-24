24ce8e037be7883bfe0fbc9717c38e8d19457f15
Vusal0013 <vusal.feyruz@gmail.com>
2023-12-24 17:56:55 +0400

    clear unused component and add classname

--- src/components/form/FormPage.tsx
+++ src/components/form/FormPage.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { ResumeForm } from "./";
-import { Stepper } from "./Stepper";
 const FormPage: React.FC = () => {
   return (
-    <div className="flex flex-col font-form">
-      {/* <Stepper steps={5} /> */}
-      <div className="bg-slate-400 w-full h-20">
+    <div className="flex flex-col font-form bg-form bg-cover bg-center w-full min-h-screen">
+      <div className="w-full h-full text-sm md:text-base">
         <ResumeForm />
       </div>
     </div>
