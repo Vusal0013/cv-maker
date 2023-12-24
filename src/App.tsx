96ff4d1dea4c8139dd4f718b4424e0e8a1e7d73a
Vusal0013 <vusal.feyruz@gmail.com>
2023-12-24 20:03:16 +0400

    added Link for resume and form pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { FormPage } from "./components/form/FormPage";
 import "./index.css";
 import { R01 } from "./pages";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Link to="/form">Form</Link>
+              <br />
+              <Link to="/resume">Resume</Link>
+            </>
+          }
+        />
         <Route path="/resume" element={<R01 />} />
         <Route path="/form" element={<FormPage />} />
       </Routes>
