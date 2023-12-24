daffda86bd5e3f09255944cb128d5c7ecdebd9c3
Vusal0013 <vusal.feyruz@gmail.com>
2023-12-24 17:58:27 +0400

    modified style and add placeholder text

--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -4,11 +4,13 @@ import { InputAnimation, InputError } from ".";
 
 export type IInput = {
   label?: string;
+  placeholder?: string;
 };
 
 const Input: React.FC<IInput & FieldHookConfig<any>> = ({
   label,
   type,
+  placeholder,
   ...props
 }) => {
   const [field, meta] = useField(props);
@@ -16,30 +18,33 @@ const Input: React.FC<IInput & FieldHookConfig<any>> = ({
   const [inputFocus, setInputFocus] = useState<boolean>(false);
 
   return (
-    <div className="relative w-full">
-      <label
-        className="text-[#2e2e2e] text-xs font-normal"
-        htmlFor={field.name}
-      >
-        {label}
-      </label>
-      <div className="relative">
-        <input
-          className="w-full pr-6 py-1 px-2 bg-gray-50 text-[#2e2e2e] border-2 outline-none border-[#cdcdcd]"
-          id={field.name}
-          type={type ?? "text"}
-          {...field}
-          value={props.value}
-          onFocus={() => setInputFocus(true)}
-          onBlurCapture={() => setInputFocus(false)}
-        />
-        <InputAnimation meta={meta} focus={inputFocus} />
-      </div>
+    <div className="w-full flex justify-center items-center">
+      <div className="relative w-full">
+        <label
+          className="text-[#2e2e2e] text-xs font-normal"
+          htmlFor={field.name}
+        >
+          {label}
+        </label>
+        <div className="relative">
+          <input
+            placeholder={placeholder}
+            className="w-full pr-6 py-1 px-2 bg-gray-50 text-[#2e2e2e] border-2 outline-none border-[#cdcdcd]"
+            id={field.name}
+            type={type ?? "text"}
+            {...field}
+            value={field.value}
+            onFocus={() => setInputFocus(true)}
+            onBlurCapture={() => setInputFocus(false)}
+          />
+          <InputAnimation meta={meta} focus={inputFocus} />
+        </div>
 
-      <ErrorMessage
-        name={field.name}
-        children={(m) => <InputError children={m} />}
-      />
+        <ErrorMessage
+          name={field.name}
+          children={(m) => <InputError children={m} />}
+        />
+      </div>
     </div>
   );
 };
