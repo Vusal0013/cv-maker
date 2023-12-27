360ddbdf1f2e0710cc7e9f053c1cd9090d9f031a
Vusal0013 <vusal.feyruz@gmail.com>
2023-12-28 00:43:55 +0400

    add focus underline and error message also added onfocus and onbluer eventHanler

--- src/components/form/SkillItem.tsx
+++ src/components/form/SkillItem.tsx
@@ -1,16 +1,18 @@
-import React, { useState } from "react";
-import { Input } from ".";
+import React, { useEffect, useState } from "react";
+import { Input, InputError } from ".";
 import { IoIosRemoveCircleOutline } from "react-icons/io";
 import { ILanguageSkill, IProfessionalSkills } from "../../types";
 import {
+  ErrorMessage,
   FieldConfig,
   FieldHelperProps,
   FieldInputProps,
   FieldMetaProps,
 } from "formik";
+import classNames from "classnames";
 
 interface ISkillItem {
-  language?: boolean;
+  language: boolean;
   handlePush: <X = any>(obj: X) => () => void;
   handleRemove: (index: number) => () => void;
   getFieldMeta: <Value>(name: string) => FieldMetaProps<Value>;
@@ -27,14 +29,35 @@ const SkillItem: React.FC<ISkillItem> = ({
   language,
   handleRemove,
   handlePush,
+  getFieldHelpers,
+  getFieldMeta,
+  getFieldProps,
   index,
   fieldName,
   fieldValue,
 }) => {
-  const [percent, setPercent] = useState<string>("0");
+  const field = (childField: string) => {
+    return getFieldProps(`${fieldName}[${index}].${childField}`);
+  };
+
+  const meta = (childField: string) => {
+    return getFieldMeta(field(childField).name);
+  };
+
+  const helpers = (childField: string) => {
+    return getFieldHelpers(field(childField).name);
+  };
+
+  const [percent, setPercent] = useState<string>(field("proficiency").value);
+
+  const [rangeInputFocus, setRangeInputFocus] = useState<boolean>(false);
+
+  useEffect(() => {
+    helpers("proficiency").setValue(percent);
+  }, [percent]);
 
   return (
-    <div className="w-full">
+    <div className="w-full relative">
       <div className="relative">
         <div
           onClick={handleRemove(index)}
@@ -43,7 +66,7 @@ const SkillItem: React.FC<ISkillItem> = ({
           <IoIosRemoveCircleOutline size={20} />
         </div>
         <Input
-          name={`${fieldName}${index}.${language ? "language" : "skillName"}`}
+          name={`${fieldName}[${index}].${language ? "language" : "skillName"}`}
           placeholder={language ? "Language" : "Skill"}
         />
       </div>
@@ -51,6 +74,8 @@ const SkillItem: React.FC<ISkillItem> = ({
         <div className="flex flex-col h-4 bg-gray-50 relative overflow-hidden border-2 border-[#cdcdcd] mt-1">
           <input
             list="percent-list"
+            onFocus={() => setRangeInputFocus(true)}
+            onBlur={() => setRangeInputFocus(false)}
             onChange={(e) => setPercent(e.target.value)}
             type="range"
             className="w-full opacity-0 z-10 cursor-pointer"
@@ -63,43 +88,59 @@ const SkillItem: React.FC<ISkillItem> = ({
             className="absolute bg-gradient-to-r from-[#81abee] to-[#362a3d] h-full border"
           ></div>
         </div>
-        <datalist className="flex justify-between text-xs" id="percent-list">
-          <option
-            onClick={(e) => setPercent((e.target as HTMLOptionElement).value)}
-            className="cursor-pointer"
-            value="0"
-          >
-            0%
-          </option>
-          <option
-            onClick={(e) => setPercent((e.target as HTMLOptionElement).value)}
-            className="cursor-pointer"
-            value="25"
-          >
-            25%
-          </option>
-          <option
-            onClick={(e) => setPercent((e.target as HTMLOptionElement).value)}
-            className="cursor-pointer"
-            value="50"
-          >
-            50%
-          </option>
-          <option
-            onClick={(e) => setPercent((e.target as HTMLOptionElement).value)}
-            className="cursor-pointer"
-            value="75"
-          >
-            75%
-          </option>
-          <option
-            onClick={(e) => setPercent((e.target as HTMLOptionElement).value)}
-            className="cursor-pointer"
-            value="100"
-          >
-            100%
-          </option>
-        </datalist>
+        <div className="relative">
+          <datalist className="flex justify-between text-xs" id="percent-list">
+            <option
+              onClick={(e) => setPercent((e.target as HTMLOptionElement).value)}
+              className="cursor-pointer"
+              value="0"
+            >
+              0%
+            </option>
+            <option
+              onClick={(e) => setPercent((e.target as HTMLOptionElement).value)}
+              className="cursor-pointer"
+              value="25"
+            >
+              25%
+            </option>
+            <option
+              onClick={(e) => setPercent((e.target as HTMLOptionElement).value)}
+              className="cursor-pointer"
+              value="50"
+            >
+              50%
+            </option>
+            <option
+              onClick={(e) => setPercent((e.target as HTMLOptionElement).value)}
+              className="cursor-pointer"
+              value="75"
+            >
+              75%
+            </option>
+            <option
+              onClick={(e) => setPercent((e.target as HTMLOptionElement).value)}
+              className="cursor-pointer"
+              value="100"
+            >
+              100%
+            </option>
+          </datalist>
+          <div
+            className={classNames({
+              "w-0 h-0.5 absolute left-0 -bottom-1 transition-all duration-500":
+                true,
+              "bg-[#cf4343] w-full":
+                meta("proficiency").touched && meta("proficiency").error,
+              "bg-[#60af68]": rangeInputFocus,
+              "w-full": rangeInputFocus,
+            })}
+          />
+        </div>
+        <ErrorMessage
+          name={`${fieldName}[${index}].proficiency`}
+          children={(m) => <InputError children={m} />}
+        />
       </div>
     </div>
   );
