6b07b3d9c2acc8f69e6078805d56399f3b8b0b8a
Vusal0013 <vusal.feyruz@gmail.com>
2023-12-30 01:39:53 +0400

    create form Step4

--- src/components/form/FormStepper/Step4.tsx
+++ src/components/form/FormStepper/Step4.tsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { Experience, Skills } from "..";
+import {
+  FieldConfig,
+  FieldHelperProps,
+  FieldInputProps,
+  FieldMetaProps,
+} from "formik";
+import { IFormType } from "../../../types";
 
-const Step4: React.FC = () => {
-  return <div>Step4</div>;
+interface IStep4Props {
+  values: IFormType;
+  getFieldMeta: <Value>(name: string) => FieldMetaProps<Value>;
+  getFieldHelpers: <Value = any>(name: string) => FieldHelperProps<Value>;
+  getFieldProps: <Value = any>(
+    props: string | FieldConfig<Value>
+  ) => FieldInputProps<Value>;
+}
+
+const Step4: React.FC<IStep4Props> = ({
+  values,
+  getFieldHelpers,
+  getFieldMeta,
+  getFieldProps,
+}) => {
+  return (
+    <>
+      <Experience
+        work
+        fieldName="professionalInfo.workExperience"
+        values={values.professionalInfo.workExperience}
+        getFieldMeta={getFieldMeta}
+        getFieldHelpers={getFieldHelpers}
+        getFieldProps={getFieldProps}
+      />
+      <Skills
+        fieldName="professionalInfo.skills"
+        values={values.professionalInfo.skills}
+        getFieldMeta={getFieldMeta}
+        getFieldHelpers={getFieldHelpers}
+        getFieldProps={getFieldProps}
+      />
+    </>
+  );
 };
 
 export { Step4 };
